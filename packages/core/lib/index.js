3dbc8d267b82f978ea31d4a5be4eee7bd5d52aaa
andy-pham-it <phamlehoaian@gmail.com>
2026-07-10 23:31:15 +0700

    feat(core): export installSkills from lib/index.js

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -2,10 +2,12 @@ const LLMClient = require('./llm');
 const BrowserManager = require('./browser');
 const Logger = require('./logger');
 const JobQueue = require('./queue');
+const { installSkills } = require('./postinstall-skills');
 
 module.exports = {
     LLMClient,
     BrowserManager,
     Logger,
     JobQueue,
+    installSkills,
 };
